d1c5ff2f76ed5e0761d0a88925547edba6cfd996
acidsound <spectrick@gmail.com>
2020-05-10 19:34:15 +0900

    feat: add Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./App.scss";
 import { Button } from "carbon-components-react";
+import { Content } from "carbon-components-react/es/components/UIShell";
+import AppHeader from "./components/AppHeader";
 export default () => {
   return (
-    <Button>Button</Button>
+    <>
+      <AppHeader />
+      <Content>
+        <Button>Button</Button>
+      </Content>
+    </>
   );
 };
